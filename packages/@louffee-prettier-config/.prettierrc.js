8993108eaad9f7f63a9bf0fc48c8a1e49265d9a2
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-07-28 00:23:58 -0300

    chore: add package to contain prettier file

---
+++ packages/@louffee-prettier-config/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 120,
+}
