f449a0a368928942b42b44aaf766bbbbb1992bc1
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-07-29 20:40:41 -0300

    refactor(button): create index file to implement exportations from the button package [ci skip]

---
+++ packages/@louffee-button/src/index.ts
@@ -0,0 +1,7 @@
+export { default as ButtonProps } from './ButtonProps'
+export { default as ButtonSize } from './ButtonSize'
+export { default as ButtonType } from './ButtonType'
+export { default as computeButtonGenericStyles } from './computeButtonGenericStyles'
+
+export { default as SoftButtonProps } from './SoftButton/SoftButtonProps'
+export { default as SoftButton } from './SoftButton/SoftButton'
