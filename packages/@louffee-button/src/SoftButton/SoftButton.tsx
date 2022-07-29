bdf763f2b78bd5fbab46f7f020ab8dbe40295840
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-07-29 19:37:57 -0300

    refactor(button): create a folder for SoftButton, implement CircularProgress for prop isLoading

--- packages/@louffee-button/src/SoftButton/SoftButton.tsx
+++ packages/@louffee-button/src/SoftButton/SoftButton.tsx
@@ -3,9 +3,10 @@ import * as React from 'react'
 import { styled, toRem } from '@louffee/style-system'
 import { useDeveloperChecks } from '@louffee/global-hooks'
 import Typography from '@louffee/typography'
+import CircularProgress from '@louffee/circular-progress'
 
-import type ButtonProps from './ButtonProps'
-import type ButtonSize from './ButtonSize'
+import type ButtonSize from '../ButtonSize'
+import type SoftButtonProps from './SoftButtonProps'
 
 const HEIGHT_PER_SIZE: { [K in ButtonSize]: string } = {
   small: toRem(24),
@@ -16,7 +17,7 @@ const HEIGHT_PER_SIZE: { [K in ButtonSize]: string } = {
 const START_ICON_CLASS_NAME = 'louffee-soft-button-start-icon'
 const END_ICON_CLASS_NAME = 'louffee-soft-button-end-icon'
 
-const StyledButton = styled('button')<ButtonProps>(({ theme, disabled, size }) => ({
+const StyledButton = styled('button')<SoftButtonProps>(({ theme, disabled, size, isLoading = false }) => ({
   color: theme.colors.primary.main,
   backgroundColor: theme.colors.primary[90],
   border: `${toRem(1)} solid ${theme.colors.primary[90]}`,
@@ -36,22 +37,29 @@ const StyledButton = styled('button')<ButtonProps>(({ theme, disabled, size }) =
 
   fontWeight: theme.typography.fontWeightMedium,
 
-  ...(disabled
-    ? {
-        pointerEvents: 'none',
-        color: theme.colors.grey[0],
-        backgroundColor: theme.colors.grey[90],
-        borderColor: theme.colors.grey[90],
-        cursor: 'not-allowed',
-      }
-    : {
-        '&:hover': {
-          backgroundColor: theme.colors.primary[100],
-          borderColor: theme.colors.primary[100],
-          cursor: 'pointer',
-          boxShadow: theme.shadows.slight,
-        },
-      }),
+  ...(disabled && {
+    color: theme.colors.grey[0],
+    backgroundColor: theme.colors.grey[90],
+    borderColor: theme.colors.grey[90],
+    cursor: 'not-allowed',
+  }),
+
+  ...((isLoading || disabled) && {
+    pointerEvents: 'none',
+
+    '&:hover': {
+      cursor: 'wait !important',
+    },
+  }),
+
+  ...(!(isLoading && disabled) && {
+    '&:hover': {
+      backgroundColor: theme.colors.primary[100],
+      borderColor: theme.colors.primary[100],
+      cursor: 'pointer',
+      boxShadow: theme.shadows.slight,
+    },
+  }),
 
   [`.${START_ICON_CLASS_NAME}`]: {
     marginRight: theme.spacing.small,
@@ -62,27 +70,42 @@ const StyledButton = styled('button')<ButtonProps>(({ theme, disabled, size }) =
   },
 }))
 
-const SoftButton: React.FC<ButtonProps> = ({
+const SoftButton: React.FC<SoftButtonProps> = ({
   children,
   className = '',
   disabled = false,
   size = 'medium',
   startIcon,
   endIcon,
+  isLoading = false,
   ...props
 }) => {
   useDeveloperChecks({ children, className, disabled, size, startIcon, endIcon }, (componentProps) => {
     if (
       // Test whether the startIcon is valid to render
       typeof componentProps.startIcon === 'string' ||
-      React.isValidElement(componentProps.startIcon) ||
-      // Test whether the endIcon is valid to render
-      typeof componentProps.endIcon === 'string' ||
+      (React.isValidElement(componentProps.startIcon) &&
+        // Test whether the endIcon is valid to render
+        typeof componentProps.endIcon === 'string') ||
       React.isValidElement(componentProps.endIcon)
     ) {
       return {
         type: 'warn',
-        message: 'The startIcon and endIcon props should not be used together.',
+        message: [
+          'The startIcon and endIcon props should not be used together.',
+          'Having both of them simultaneously might distract the user from taking the action due to the overdose of UI distracting elements.',
+        ].join(' '),
+      }
+    }
+
+    if (
+      // Validate whether the children is either a string or a React element
+      typeof componentProps.children !== 'string' &&
+      !React.isValidElement(componentProps.children)
+    ) {
+      return {
+        type: 'error',
+        message: 'The children prop should be a string or a valid React element.',
       }
     }
   })
@@ -96,6 +119,11 @@ const SoftButton: React.FC<ButtonProps> = ({
       aria-disabled={disabled}>
       <Typography variant={size === 'small' ? 'bodySmall' : 'labelMedium'} aria-label={children?.toString()}>
         <i className={START_ICON_CLASS_NAME}>{startIcon}</i>
+        {isLoading && (
+          <i className={START_ICON_CLASS_NAME}>
+            <CircularProgress size={12} radii={5} thickness={2} />
+          </i>
+        )}
         {children}
         <i className={END_ICON_CLASS_NAME}>{endIcon}</i>
       </Typography>
---
+++ packages/@louffee-button/src/SoftButton/SoftButtonProps.ts
@@ -0,0 +1,7 @@
+import ButtonProps from '../ButtonProps'
+
+interface SoftButtonProps extends ButtonProps {
+  isLoading?: boolean
+}
+
+export default SoftButtonProps
