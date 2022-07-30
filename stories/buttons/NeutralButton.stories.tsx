08b3181c6bbef31af8f0014f538956782e785f9f
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-07-30 15:07:22 -0300

    test(stories): add stories for NeutralButton

---
+++ stories/buttons/NeutralButton.stories.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { ThemeProvider } from '@louffee/style-system'
+
+import NeutralButton from '../../packages/@louffee-button/src/NeutralButton/NeutralButton'
+
+export default {
+  title: 'Components/Buttons/NeutralButton',
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+  args: {
+    children: 'My Button',
+    type: 'button',
+    disabled: false,
+    size: 'medium',
+  },
+} as ComponentMeta<typeof NeutralButton>
+
+const OutlinedTemplate: ComponentStory<typeof NeutralButton> = ({
+  children,
+  type,
+  disabled,
+  size,
+  startIcon,
+  endIcon,
+  onClick,
+}) => (
+  <NeutralButton type={type} disabled={disabled} size={size} startIcon={startIcon} endIcon={endIcon} onClick={onClick}>
+    {children}
+  </NeutralButton>
+)
+
+export const OutlinedBehaviour: ComponentStory<typeof NeutralButton> = OutlinedTemplate.bind({})
+OutlinedBehaviour.storyName = 'Neutral behaviour'
