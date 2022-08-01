d5639de149be8649f7d251a465d71114070fbfe2
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-07-31 23:20:18 -0300

    test(stories): create stories for component Input

---
+++ stories/forms/Input.stories.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { ThemeProvider } from '@louffee/style-system'
+import Icon from '@louffee/icon'
+
+import Input from '../../packages/@louffee-input/src/Input'
+
+export default {
+  title: 'Components/Forms/Input',
+  component: Input,
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+} as ComponentMeta<typeof Input>
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />
+
+export const Default: ComponentStory<typeof Input> = Template.bind({})
+
+export const WithLabel: ComponentStory<typeof Input> = Template.bind({})
+WithLabel.storyName = 'With label'
+WithLabel.args = {
+  label: 'Label',
+}
+
+export const WithStartAdornment: ComponentStory<typeof Input> = Template.bind({})
+WithStartAdornment.storyName = 'With start adornment'
+WithStartAdornment.args = {
+  startAdornment: <Icon name="book" size={18} />,
+}
+
+export const WithEndAdornment: ComponentStory<typeof Input> = Template.bind({})
+WithEndAdornment.storyName = 'With end adornment'
+WithEndAdornment.args = {
+  endAdornment: <Icon name="expand-more" size={18} />,
+}
