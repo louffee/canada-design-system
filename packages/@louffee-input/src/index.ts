dc9755566ba00a67f3d65e57e81b6d07d16864d6
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-08-02 21:33:23 -0300

    refactor(input): add index file to export input-related files

---
+++ packages/@louffee-input/src/index.ts
@@ -0,0 +1,7 @@
+import Input from './Input'
+
+export { default as inputConstants } from './inputConstants'
+export { default as InputProps } from './InputProps'
+export { default as InputSize } from './InputSize'
+
+export default Input
