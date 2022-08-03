4840fae711d329c534ad0568a59fbc2e1d79730b
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-08-02 23:18:09 -0300

    chore: fix source constant pointing to the read index.css generated by utility class package

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,13 @@
 const postcss = require('postcss')
 const stylelint = require('stylelint')
 const syntax = require('postcss-syntax')
+const fs = require('fs')
+
+const source = fs.readFileSync('./packages/@louffee-utility-classes/index.css', 'utf8')
 
 postcss([stylelint({ fix: true })])
   .process(source, { syntax })
   .then(function (result) {
     // An alias for the result.css property. Use it with syntaxes that generate non-CSS output.
-    result.content
+    console.log('PostCSS has successfully processed the result!')
   })
