ac4bcb7ed85103317a5c76ebaa814caa7546bda3
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-08-04 00:00:32 -0300

    test(stories): add a few more stories for utility classes

--- stories/non-components/UtilityClasses.stories.tsx
+++ stories/non-components/UtilityClasses.stories.tsx
@@ -13,5 +13,29 @@ export const BackgroundColors: ComponentStory<never> = () => (
 )
 
 export const Padding: ComponentStory<never> = () => (
-  <div className="background-primary-main p-16">I should have a 1rem (aka 16px) padding on all sides</div>
+  <div>
+    <div className="bg-secondary-50">I've got no padding</div>
+    <div className="bg-grey-30 p-12">I've got a padding of 12px</div>
+    <div className="bg-grey-main p-30">I've got a padding of 30px</div>
+    <div className="bg-primary-main p-64">I've got a padding of 64px</div>
+  </div>
+)
+
+export const Margin: ComponentStory<never> = () => (
+  <div>
+    <div className="bg-secondary-50">I've got no margin</div>
+    <div className="bg-grey-30 m-12">I've got a margin of 12px</div>
+    <div className="bg-grey-main m-30">I've got a margin of 30px</div>
+    <div className="bg-primary-main m-64">I've got a margin of 64px</div>
+  </div>
+)
+
+export const Border: ComponentStory<never> = () => (
+  <div>
+    <div className="bg-secondary-50">I've got no border</div>
+    <div className="border-primary-10 border-solid bg-primary-80 border-1">I've got a border of 1px</div>
+    <div className="border-primary-10 border-solid bg-grey-30 border-12">I've got a border of 12px</div>
+    <div className="border-primary-10 border-solid bg-grey-main border-30">I've got a border of 30px</div>
+    <div className="border-primary-10 border-solid bg-primary-main border-64">I've got a border of 64px</div>
+  </div>
 )
