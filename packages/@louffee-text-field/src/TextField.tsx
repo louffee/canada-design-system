a93a5cb3205b34d1308648bae42cb2ca5184fed9
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-08-06 00:30:35 -0300

    refactor(text-field): implement design changes to the state of error in the text-field

--- packages/@louffee-text-field/src/TextField.tsx
+++ packages/@louffee-text-field/src/TextField.tsx
@@ -3,34 +3,42 @@ import * as React from 'react'
 
 import { useField } from '@louffee/form'
 import { useTheme } from '@louffee/style-system'
-import Input from '@louffee/input'
+import Icon from '@louffee/icon'
 import Typography from '@louffee/typography'
+import Input from '@louffee/input'
 
 import type TextFieldProps from './TextFieldProps'
 
 const TextField = React.memo<TextFieldProps>(
-  ({ name, startAdornment, endAdornment, size = 'medium', label, format, parse, ...props }) => {
+  ({ name, startAdornment, endAdornment, size = 'medium', label, format, parse, autoComplete = 'off', ...props }) => {
     const { attributes, meta } = useField(name, { parse, format })
     const {
       colors: { error },
     } = useTheme()
 
     return (
-      <>
+      <div className="louffee-text-field flex flex-col g-3">
         <Input
           {...props}
           {...attributes}
+          autoComplete={autoComplete}
           label={label}
           size={size}
           startAdornment={startAdornment}
           endAdornment={endAdornment}
         />
         {typeof meta.error === 'string' && meta.error.length > 1 && (
-          <Typography variant="bodySmall" color={error.main}>
-            {meta.error}
-          </Typography>
+          <div className="flex align-center radii-6 mt-3 bg-error-light px-6 per-width-100 left--2 relative">
+            <div className="mx-3 flex align-center justify-center p-3 m-4 radii-5 bg-primary-100">
+              <Icon name="lock" color={error.main} />
+            </div>
+
+            <Typography variant="bodySmall" color={error.main}>
+              {meta.error}
+            </Typography>
+          </div>
         )}
-      </>
+      </div>
     )
   }
 )
