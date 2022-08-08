17ef892e7834e8c96b74c903f507cad4260a4266
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-08-08 01:42:14 -0300

    refactor(global-hooks): implmement useCallback in useWindowSize updateSize() function

--- packages/@louffee-global-hooks/src/useWindowSize/useWindowSize.ts
+++ packages/@louffee-global-hooks/src/useWindowSize/useWindowSize.ts
@@ -1,22 +1,24 @@
 import * as React from 'react'
 
 import useEffect from '../useEffect/useEffect'
+import useCallback from '../useCallback/useCallback'
 
 import type UseWindowSizeReturnType from './UseWindowSizeReturnType'
 
 function useWindowSize(): UseWindowSizeReturnType {
   const [size, setSize] = React.useState<UseWindowSizeReturnType>({ height: 0, width: 0 })
 
-  useEffect(() => {
-    function updateSize() {
-      setSize({ height: window.innerHeight, width: window.innerWidth })
-    }
+  const updateSize = useCallback(() => {
+    setSize({ height: window.innerHeight, width: window.innerWidth })
+  }, [setSize])
 
+  useEffect(() => {
     window.addEventListener('resize', updateSize)
 
     return () => {
       window.removeEventListener('resize', updateSize)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return size
