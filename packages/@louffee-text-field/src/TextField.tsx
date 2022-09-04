8a9d677858b4f958c19e248eab80dc692ba59083
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-04 02:02:23 -0300

    refactor: adapt the TextField component to the changes in the Input component (base thereof)

--- packages/@louffee-text-field/src/TextField.tsx
+++ packages/@louffee-text-field/src/TextField.tsx
@@ -1,20 +1,13 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import * as React from 'react'
 
 import { useField } from '@louffee/canada-form'
-import { useTheme } from '@louffee/canada-style-system'
-import Icon from '@louffee/canada-icon'
-import Typography from '@louffee/canada-typography'
 import Input from '@louffee/canada-input'
 
 import type TextFieldProps from './TextFieldProps'
 
 const TextField = React.memo<TextFieldProps>(
-  ({ name, startAdornment, endAdornment, size = 'medium', label, format, parse, autoComplete = 'off', ...props }) => {
+  ({ name, startAdornment, endAdornment, label, format, parse, autoComplete = 'off', ...props }) => {
     const { attributes, meta } = useField(name, { parse, format })
-    const {
-      colors: { error },
-    } = useTheme()
 
     const shouldRenderError = React.useMemo<boolean>(
       () =>
@@ -27,28 +20,15 @@ const TextField = React.memo<TextFieldProps>(
     )
 
     return (
-      <div className='louffee-text-field flex flex-col g-3'>
-        <Input
-          {...props}
-          {...attributes}
-          autoComplete={autoComplete}
-          label={label}
-          size={size}
-          startAdornment={startAdornment}
-          endAdornment={endAdornment}
-        />
-        {shouldRenderError && (
-          <div className='flex align-center radii-6 mt-3 bg-error-light px-6 per-width-100 left--2 relative'>
-            <div className='mx-3 flex align-center justify-center p-3 m-4 radii-5 bg-primary-100'>
-              <Icon name='lock' color={error.main} />
-            </div>
-
-            <Typography variant='bodySmall' color={error.main}>
-              {meta.error}
-            </Typography>
-          </div>
-        )}
-      </div>
+      <Input
+        {...props}
+        {...attributes}
+        autoComplete={autoComplete}
+        label={label}
+        startAdornment={startAdornment}
+        endAdornment={endAdornment}
+        error={shouldRenderError ? meta.error : undefined}
+      />
     )
   },
 )
