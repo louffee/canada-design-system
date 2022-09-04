a28da28260db94bab2ef77d97cdc024f5a550a1b
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-04 02:01:59 -0300

    refactor: synchronize the exportation file with the changes in input component

--- packages/@louffee-input/src/index.ts
+++ packages/@louffee-input/src/index.ts
@@ -1,6 +1,5 @@
 import Input from './Input'
 
-export { default as inputConstants } from './inputConstants'
 export { default as InputProps } from './InputProps'
 export { default as InputSize } from './InputSize'
 
