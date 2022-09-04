79683a436183c1e6910681aa6af89e80ab83cb68
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-04 01:53:56 -0300

    test(sb/forms): change stories to comport the new input component

--- stories/forms/Input.stories.tsx
+++ stories/forms/Input.stories.tsx
@@ -7,13 +7,17 @@ import Icon from '@louffee/canada-icon'
 
 import Input from '../../packages/@louffee-input/src/Input'
 
+import '@louffee/canada-utility-classes'
+
 export default {
   title: 'Components/Forms/Input',
   component: Input,
   decorators: [
     (Story) => (
       <ThemeProvider>
-        <Story />
+        <main className='margin-x-auto max-width-800'>
+          <Story />
+        </main>
       </ThemeProvider>
     ),
   ],
@@ -26,17 +30,24 @@ export const Default: ComponentStory<typeof Input> = Template.bind({})
 export const WithLabel: ComponentStory<typeof Input> = Template.bind({})
 WithLabel.storyName = 'With label'
 WithLabel.args = {
-  label: 'Label',
+  label: 'Please enter your name',
 }
 
 export const WithStartAdornment: ComponentStory<typeof Input> = Template.bind({})
 WithStartAdornment.storyName = 'With start adornment'
 WithStartAdornment.args = {
-  startAdornment: <Icon name="book" size={18} />,
+  startAdornment: <Icon name='book' size={18} />,
 }
 
 export const WithEndAdornment: ComponentStory<typeof Input> = Template.bind({})
 WithEndAdornment.storyName = 'With end adornment'
 WithEndAdornment.args = {
-  endAdornment: <Icon name="expand-more" size={18} />,
+  endAdornment: <Icon name='expand-more' size={18} />,
+}
+
+export const WithError: ComponentStory<typeof Input> = Template.bind({})
+WithError.storyName = 'With error'
+WithError.args = {
+  error: 'This is an example of an error',
+  label: 'Please enter your name',
 }
