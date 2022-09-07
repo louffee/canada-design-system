3e884b78abc66b94eccfcb55d2fcd8fd5808d34b
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-07 11:36:29 -0300

    test(sb/forms): add a few more stories in the Form stories file

--- stories/forms/Form.stories.tsx
+++ stories/forms/Form.stories.tsx
@@ -2,14 +2,18 @@ import * as React from 'react'
 
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
-import '@louffee/canada-utility-classes'
 import { ThemeProvider } from '@louffee/canada-style-system'
+import { SoftButton } from '@louffee/canada-button'
 
+import useFormState from '../../packages/@louffee-form/src/useFormState/useFormState'
 import useField from '../../packages/@louffee-form/src/useField/useField'
 import createValidation from '../../packages/@louffee-form/src/createValidation'
 import errorSchema from '../../packages/@louffee-form/src/errorSchema'
 import Form from '../../packages/@louffee-form/src/Form'
 import TextField from '../../packages/@louffee-text-field/src/TextField'
+import * as masks from '../../packages/@louffee-masks/src/index'
+
+import '@louffee/canada-utility-classes'
 
 interface Values {
   firstName: string
@@ -77,10 +81,49 @@ export const Default: ComponentStory<typeof Form> = () => {
 export const PersonForm: ComponentStory<typeof Form> = () => (
   <div>
     <div>
-      <TextField name="firstName" label="First name" placeholder="John e.g." />
+      <TextField name='firstName' label='First name' placeholder='John e.g.' />
     </div>
     <div>
-      <TextField name="lastName" label="Last name" placeholder="Doe e.g." />
+      <TextField name='lastName' label='Last name' placeholder='Doe e.g.' />
     </div>
   </div>
 )
+
+export const AddressForm: ComponentStory<typeof Form> = () => {
+  type FormValues = {
+    addressLine1?: string
+    addressLine2?: string
+    phoneCountryCode?: string
+    phoneNumber?: string
+  }
+
+  const { values, forceSubmit } = useFormState<FormValues>()
+
+  return (
+    <div className='flex flex-col max-width-800 margin-x-auto gap-10'>
+      <div className='flex align-center gap-10'>
+        <div className='flex-2'>
+          <TextField name='addressLine1' label='Please enter your address line' />
+        </div>
+        <div className='flex-1'>
+          <TextField name='addressLine2' label='Please enter your address line (optional)' />
+        </div>
+      </div>
+
+      <TextField name='city' label='Enter the name of the city where you live' />
+      <div className='flex align-center gap-10'>
+        <div>
+          <TextField name='phoneCountryCode' mask={masks.phoneCountryCode} label='Phone country code' />
+        </div>
+        <div className='flex-1'>
+          <TextField name='phone' mask={masks.phone(values.phoneCountryCode)} label='Enter your phone' />
+        </div>
+      </div>
+      <div className='flex justify-end'>
+        <SoftButton type='submit' onClick={forceSubmit}>
+          Submit
+        </SoftButton>
+      </div>
+    </div>
+  )
+}
