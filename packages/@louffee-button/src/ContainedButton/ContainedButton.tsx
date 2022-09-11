c1d911e35bc288140e481497057deec92f939bba
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-11 19:57:20 -0300

    feat: create contained button
    Re #54

---
+++ packages/@louffee-button/src/ContainedButton/ContainedButton.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react'
+import { styled } from '@louffee/canada-style-system'
+import Typography from '@louffee/canada-typography'
+
+import buttonConstants from '../buttonConstants'
+import computeButtonGenericStyles from '../computeButtonGenericStyles'
+
+import type ContainedButtonProps from './ContainedButtonProps'
+
+const StyledButton = styled('button')<ContainedButtonProps>(({ disabled, fullWidth, theme }) => {
+  const genericStyles = computeButtonGenericStyles(theme, { disabled, fullWidth })
+
+  return {
+    ...genericStyles,
+
+    borderWidth: 1,
+    borderStyle: 'solid',
+    backgroundColor: theme.colors.primary[20],
+    borderColor: theme.colors.primary[20],
+    color: theme.colors.white,
+
+    '&:hover': {
+      backgroundColor: theme.colors.primary[30],
+      borderColor: theme.colors.primary[30],
+    },
+
+    ...(disabled && {
+      color: theme.colors.grey[80],
+      backgroundColor: theme.colors.grey[95],
+      borderColor: theme.colors.grey[90],
+    }),
+  }
+})
+
+const ContainedButton = React.memo<ContainedButtonProps>(
+  ({ children, disabled = false, className = '', startIcon, endIcon, fullWidth = false, ...props }) => (
+    <StyledButton
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      className={`louffee-outlined-button ${className}`}
+      fullWidth={fullWidth}
+      disabled={disabled}
+      aria-disabled={disabled}>
+      <Typography variant='bodySmall' aria-label={children?.toString()}>
+        <i className={buttonConstants.START_ICON_CLASS_NAME}>{startIcon}</i>
+        {children}
+        <i className={buttonConstants.END_ICON_CLASS_NAME}>{endIcon}</i>
+      </Typography>
+    </StyledButton>
+  ),
+)
+
+ContainedButton.displayName = 'ContainedButton'
+
+export default ContainedButton
