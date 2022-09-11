a85f188b9a559a0482921f29c29b609297793164
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-10 23:03:50 -0300

    refactor(global-hooks): update code based on recommendations from the linter

--- packages/@louffee-global-hooks/src/useLocale/multiChannelLocaleSearch.ts
+++ packages/@louffee-global-hooks/src/useLocale/multiChannelLocaleSearch.ts
@@ -3,10 +3,11 @@ import type SupportedLocale from './SupportedLocale'
 function fromLocationSearch(): SupportedLocale | undefined {
   let supportedLocale: SupportedLocale | undefined
 
-  const params = new URLSearchParams(window.location.search)
+  // eslint-disable-next-line compat/compat
+  const parameters = new URLSearchParams(window.location.search)
 
-  if (params.has('lang')) {
-    const lang = params.get('lang')
+  if (parameters.has('lang')) {
+    const lang = parameters.get('lang')
 
     if (lang) {
       supportedLocale = lang.replace('-', '_') as SupportedLocale
@@ -17,13 +18,13 @@ function fromLocationSearch(): SupportedLocale | undefined {
 }
 
 function fromNavigation(): SupportedLocale | undefined {
-  if (typeof window.navigator?.language === 'string' && window.navigator.language.length >= 1) {
+  if (typeof window.navigator?.language === 'string' && window.navigator.language.length > 0) {
     return window.navigator.language.replace('-', '_') as SupportedLocale
   }
 
   if (
     Array.isArray(window.navigator?.languages) &&
-    window.navigator.languages.length >= 1 &&
+    window.navigator.languages.length > 0 &&
     typeof window.navigator.languages[0] === 'string'
   ) {
     return window.navigator.languages[0].replace('-', '_') as SupportedLocale
