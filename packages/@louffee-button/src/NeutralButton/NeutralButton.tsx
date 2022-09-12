685d555ec01a479bb248b62de59bf0315eb646e1
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-12 20:02:02 -0300

    build(ts): remove ts-expect-error annotation from neutral-button component

--- packages/@louffee-button/src/NeutralButton/NeutralButton.tsx
+++ packages/@louffee-button/src/NeutralButton/NeutralButton.tsx
@@ -10,7 +10,6 @@ import type NeutralButtonProps from './NeutralButtonProps'
 // Unfortunately we've got an overlapping problem with the native attributes and
 // the OutlinedButtonProps we couldn't solve yet.
 // todo: fix this somehow =/
-// @ts-expect-error
 const StyledButton = styled('button')<NeutralButtonProps>(({ theme, size, disabled, fullWidth }) => {
   const genericStyles = computeButtonGenericStyles(theme, { size, disabled, fullWidth })
 
