4afa33bc775b03198b678b7f951eef37d59514e9
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-16 22:39:31 -0300

    refactor(tooltip): lower tooltip background opacity from 78% to 70%

--- packages/@louffee-tooltip/src/tooltipConstants.ts
+++ packages/@louffee-tooltip/src/tooltipConstants.ts
@@ -1,5 +1,5 @@
 const tooltipConstants = {
-  BACKGROUND_COLOR: 'rgba(255, 255, 255, 0.78)',
+  BACKGROUND_COLOR: 'rgba(255, 255, 255, 0.70)',
 
   CONTENT_ANIMATION_TIMING_FUNCTION: 'cubic-bezier(0.23, 1, 0.32, 1)',
   CONTENT_ANIMATION_DURATION: '0.6s',
