38b5543266c8cb567387d3e115b8e5d14c29cb3e
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-16 23:48:49 -0300

    refactor(button): transport the styles for disabled state to button-shared styles and align icons

--- packages/@louffee-button/src/computeButtonGenericStyles.ts
+++ packages/@louffee-button/src/computeButtonGenericStyles.ts
@@ -2,6 +2,7 @@ import { toRem, LouTheme, type CSSObject } from '@louffee/canada-style-system'
 
 import type ButtonSize from './ButtonSize'
 import type ButtonProps from './ButtonProps'
+import buttonConstants from './buttonConstants'
 
 const HEIGHT_PER_SIZE: { [K in ButtonSize]: string } = {
   small: toRem(24),
@@ -37,9 +38,25 @@ const computeButtonGenericStyles = (
   height: HEIGHT_PER_SIZE[size],
 
   ...(disabled && {
+    color: theme.colors.grey[80],
+    backgroundColor: theme.colors.grey[95],
+    borderColor: theme.colors.grey[90],
     cursor: 'not-allowed',
     pointerEvents: 'none',
   }),
+
+  [`.${buttonConstants.START_ICON_CLASS_NAME}, .${buttonConstants.END_ICON_CLASS_NAME}`]: {
+    position: 'relative',
+    top: 2,
+  },
+
+  [`.${buttonConstants.START_ICON_CLASS_NAME}`]: {
+    marginRight: toRem(4),
+  },
+
+  [`.${buttonConstants.END_ICON_CLASS_NAME}`]: {
+    marginLeft: toRem(4),
+  },
 })
 
 export default computeButtonGenericStyles
