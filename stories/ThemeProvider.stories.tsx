3ba357fbc81ad34d949d41af17530285624e10b2
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-19 02:12:30 -0300

    test(stories): improve ThemeProvider colour schema UI in storybook

--- stories/ThemeProvider.stories.tsx
+++ stories/ThemeProvider.stories.tsx
@@ -1,38 +1,67 @@
-// @louffee/canada-style-system -> ThemeProvider
 import * as React from 'react'
-
+import Typography from '@louffee/canada-typography'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 import useTheme from '../packages/@louffee-style-system/src/useTheme'
+import styled from '../packages/@louffee-style-system/src/styled'
 import ThemeProvider from '../packages/@louffee-style-system/src/ThemeProvider'
+
+import '@louffee/canada-utility-classes'
 
 export default {
   title: 'Components/ThemeProvider',
   component: ThemeProvider,
 } as ComponentMeta<typeof ThemeProvider>
 
+const Container = styled('main')(({ theme }) => ({
+  backgroundColor: 'rgba(0, 0, 0, 0.025)',
+  paddingInline: theme.spacing.large,
+  borderRadius: theme.radii.medium,
+  boxShadow: theme.shadows.slight,
+
+  display: 'flex',
+  flexDirection: 'column',
+  flexWrap: 'nowrap',
+}))
+
+const ColourGrid = styled('div')(({ theme }) => ({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(8, 1fr)',
+  margin: theme.spacing.extraSmall,
+}))
+
 const ThemeConsumer: React.FC = () => {
-  const theme = useTheme()
+  const { colors } = useTheme()
+
+  const rectangleSize = React.useMemo(() => 80, [])
 
   return (
-    <div id="theme-consumer-render">
-      {Object.entries(theme.colors).map(([paletteName, value]) => (
-        <div key={paletteName}>
-          <div>
+    <Container id='theme-consumer-render'>
+      {Object.entries(colors).map(([paletteName, value]) => (
+        <div className='flex flex-col' key={paletteName}>
+          <Typography variant='headlineSmall'>{paletteName}</Typography>
+
+          <ColourGrid>
             {Object.entries(value).map(([colorScale, colorValue]) => (
-              <ul key={colorScale}>
-                <div>
-                  {paletteName}:{colorScale}
+              <div key={`#${paletteName}[${colorScale}]`}>
+                <div className='flex align-center'>
+                  <Typography variant='bodyLarge'>Variant</Typography>
+                  &nbsp;
+                  <Typography variant='titleMedium'>{colorScale}</Typography>
                 </div>
-                <svg>
-                  <rect width={300} height={300} fill={colorValue} strokeWidth={4} stroke="#ccc" strokeMiterlimit={1} />
+                <svg
+                  fill={colorValue}
+                  viewBox={`0 0 ${rectangleSize} ${rectangleSize}`}
+                  height={rectangleSize}
+                  width={rectangleSize}>
+                  <rect height={rectangleSize} width={rectangleSize} />
                 </svg>
-              </ul>
+              </div>
             ))}
-          </div>
+          </ColourGrid>
         </div>
       ))}
-    </div>
+    </Container>
   )
 }
 
