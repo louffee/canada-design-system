7b5631c5e62ea2f6a91b45c19d11fdb4a1542832
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-19 02:11:36 -0300

    test(stories/_internal): use Canada's styled API for CopyrightFooter internal component for stories

--- stories/_internal/CopyrightFooter.tsx
+++ stories/_internal/CopyrightFooter.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react'
 
+import { styled } from '@louffee/canada-style-system'
+
+const SmallText = styled('small')({
+  backgroundColor: 'rgba(0, 0, 0, 0.03)',
+  borderRadius: 6,
+  width: '100vw',
+  padding: '1rem',
+  pointerEvents: 'none',
+})
+
 const CopyrightFooter = (): React.ReactElement => (
   <>
     <br />
     <br />
 
-    <small
-      style={{
-        backgroundColor: 'rgba(0, 0, 0, 0.03)',
-        borderRadius: 6,
-        width: '100vw',
-        padding: '1rem',
-        pointerEvents: 'none',
-      }}>
-      Copyright &copy; Louffee, {new Date().getFullYear()}. All rights reserved.
-    </small>
+    <SmallText>Copyright &copy; Louffee, {new Date().getFullYear()}. All rights reserved.</SmallText>
   </>
 )
 
