555d561ace78556d3edbadfe919be7043f6ff521
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-19 04:28:21 -0300

    test(stories): create file to contain stories with the catalogue of typography variants

---
+++ stories/miscellaneous/Typography.stories.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react'
+import { ThemeProvider, styled } from '@louffee/canada-style-system'
+import { ComponentStory, type ComponentMeta } from '@storybook/react'
+
+import Typography from '../../packages/@louffee-typography/src/Typography'
+import elementsPerVariantMap from '../../packages/@louffee-typography/src/elementsPerVariantMap'
+import type TypographyVariant from '../../packages/@louffee-typography/src/TypographyVariant'
+
+import '@louffee/canada-utility-classes'
+
+export default {
+  title: 'Components/Miscellaneous/Typography',
+  component: Typography,
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+} as ComponentMeta<typeof Typography>
+
+const VARIANTS = Object.keys(elementsPerVariantMap) as TypographyVariant[]
+
+const Snippet = styled('div')(({ theme }) => ({
+  backgroundColor: theme.colors.grey[99],
+  padding: theme.spacing.extraLarge,
+  borderRadius: theme.radii.medium,
+  boxShadow: theme.shadows.slight,
+
+  minHeight: 100,
+}))
+
+const CodeTag = styled('code')(({ theme }) => ({
+  color: theme.colors.primary.main,
+  backgroundColor: theme.colors.white,
+  padding: theme.spacing.medium,
+  borderRadius: theme.radii.small,
+  fontFamily: 'monospace !important',
+  width: 'fit-content',
+}))
+
+export const Catalogue: ComponentStory<typeof Typography> = (args) => (
+  <div className='max-width-900 margin-x-auto flex flex-col gap-24'>
+    {VARIANTS.map((variant) => (
+      <Snippet key={variant} className='flex flex-col gap-4'>
+        <CodeTag>{variant}</CodeTag>
+        <br />
+        <Typography {...args} variant={variant}>
+          Lorem ipsum dolor sit amet.
+        </Typography>
+      </Snippet>
+    ))}
+  </div>
+)
