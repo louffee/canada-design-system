f3f6f29c5020f0b00c707ac1d66397c161a456d4
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-19 04:45:18 -0300

    test(stories): fix the changes to show colours when non-object schema in theme

--- stories/ThemeProvider.stories.tsx
+++ stories/ThemeProvider.stories.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import Typography from '@louffee/canada-typography'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { capitalize } from 'quebec'
 
 import useTheme from '../packages/@louffee-style-system/src/useTheme'
 import styled from '../packages/@louffee-style-system/src/styled'
 import ThemeProvider from '../packages/@louffee-style-system/src/ThemeProvider'
+import type LouTheme from '../packages/@louffee-style-system/src/LouTheme'
 
 import '@louffee/canada-utility-classes'
 
@@ -16,8 +18,9 @@ export default {
 const Container = styled('main')(({ theme }) => ({
   backgroundColor: 'rgba(0, 0, 0, 0.025)',
   paddingInline: theme.spacing.large,
-  borderRadius: theme.radii.medium,
   boxShadow: theme.shadows.slight,
+
+  borderRadius: theme.radii.medium,
 
   display: 'flex',
   flexDirection: 'column',
@@ -30,34 +33,42 @@ const ColourGrid = styled('div')(({ theme }) => ({
   margin: theme.spacing.extraSmall,
 }))
 
-const ThemeConsumer: React.FC = () => {
-  const { colors } = useTheme()
+const RECTANGLE_SIZE = 80
 
-  const rectangleSize = React.useMemo(() => 80, [])
+const renderColourBlock = (colour: string, theme: typeof LouTheme): React.ReactElement => (
+  <svg
+    fill={colour}
+    viewBox={`0 0 ${RECTANGLE_SIZE} ${RECTANGLE_SIZE}`}
+    height={RECTANGLE_SIZE}
+    width={RECTANGLE_SIZE}
+    strokeWidth={1}
+    stroke={theme.colors.grey[80]}>
+    <rect height={RECTANGLE_SIZE} width={RECTANGLE_SIZE} />
+  </svg>
+)
+
+const ThemeConsumer: React.FC = () => {
+  const theme = useTheme()
 
   return (
     <Container id='theme-consumer-render'>
-      {Object.entries(colors).map(([paletteName, value]) => (
+      {Object.entries(theme.colors).map(([paletteName, value]) => (
         <div className='flex flex-col' key={paletteName}>
-          <Typography variant='headlineSmall'>{paletteName}</Typography>
+          <Typography variant='headlineSmall'>{capitalize(paletteName)}</Typography>
 
           <ColourGrid>
-            {Object.entries(value).map(([colorScale, colorValue]) => (
-              <div key={`#${paletteName}[${colorScale}]`}>
-                <div className='flex align-center'>
-                  <Typography variant='bodyLarge'>Variant</Typography>
-                  &nbsp;
-                  <Typography variant='titleMedium'>{colorScale}</Typography>
+            {typeof value === 'object' &&
+              Object.entries(value).map(([colorScale, colorValue]) => (
+                <div key={`#${paletteName}[${colorScale}]`}>
+                  <div className='flex align-center'>
+                    <Typography variant='bodyLarge'>Variant</Typography>
+                    &nbsp;
+                    <Typography variant='titleMedium'>{colorScale}</Typography>
+                  </div>
+                  {renderColourBlock(colorValue, theme)}
                 </div>
-                <svg
-                  fill={colorValue}
-                  viewBox={`0 0 ${rectangleSize} ${rectangleSize}`}
-                  height={rectangleSize}
-                  width={rectangleSize}>
-                  <rect height={rectangleSize} width={rectangleSize} />
-                </svg>
-              </div>
-            ))}
+              ))}
+            {typeof value === 'string' && renderColourBlock(value, theme)}
           </ColourGrid>
         </div>
       ))}
