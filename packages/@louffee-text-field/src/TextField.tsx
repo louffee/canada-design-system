bfac89983641572f1084c33b19f41e4351031a79
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-23 01:35:48 -0300

    refactor(text-field): compose event handlers from prop and form field, use hooks from global-hooks

--- packages/@louffee-text-field/src/TextField.tsx
+++ packages/@louffee-text-field/src/TextField.tsx
@@ -1,14 +1,35 @@
 import * as React from 'react'
 import { useField } from '@louffee/canada-form'
+import { useMemo, useCallback } from '@louffee/canada-global-hooks'
 import Input from '@louffee/canada-input'
 
 import type TextFieldProps from './TextFieldProps'
 
 const TextField = React.memo<TextFieldProps>(
-  ({ name, startAdornment, endAdornment, label, format, parse, mask, autoComplete = 'off', onKeyDown, ...props }) => {
-    const { attributes, meta } = useField(name, { parse: mask?.parse ?? parse, format: mask?.format ?? format })
+  ({
+    name,
+    startAdornment,
+    endAdornment,
+    label,
+    format,
+    parse,
+    mask,
+    autoComplete = 'off',
+    onKeyDown,
+    onChange: onChangeProperty,
+    onBlur: onBlurProperty,
+    onFocus: onFocusProperty,
+    ...props
+  }) => {
+    const {
+      attributes: { onChange, onBlur, onFocus, ...attributes },
+      meta,
+    } = useField(name, {
+      parse: mask?.parse ?? parse,
+      format: mask?.format ?? format,
+    })
 
-    const shouldRenderError = React.useMemo<boolean>(
+    const shouldRenderError = useMemo<boolean>(
       () =>
         // Checks whether the user has interacted with the field
         meta.touched &&
@@ -18,7 +39,31 @@ const TextField = React.memo<TextFieldProps>(
       [meta.touched, meta.error],
     )
 
-    const handleKeyDown = React.useCallback(
+    const handleChange = useCallback(
+      (event: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(event)
+        onChangeProperty?.(event)
+      },
+      [onChange, onChangeProperty],
+    )
+
+    const handleBlur = useCallback(
+      (event: React.FocusEvent<HTMLInputElement>) => {
+        onBlur(event)
+        onBlurProperty?.(event)
+      },
+      [onBlur, onBlurProperty],
+    )
+
+    const handleFocus = useCallback(
+      (event: React.FocusEvent<HTMLInputElement>) => {
+        onFocus(event)
+        onFocusProperty?.(event)
+      },
+      [onFocus, onFocusProperty],
+    )
+
+    const handleKeyDown = useCallback(
       (event?: React.KeyboardEvent<HTMLInputElement>) => {
         if (typeof onKeyDown === 'function') {
           onKeyDown(event)
@@ -39,6 +84,9 @@ const TextField = React.memo<TextFieldProps>(
         /* eslint-enable react/jsx-props-no-spreading */
         autoComplete={autoComplete}
         label={label}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
         startAdornment={startAdornment}
         endAdornment={endAdornment}
         onKeyDown={handleKeyDown}
