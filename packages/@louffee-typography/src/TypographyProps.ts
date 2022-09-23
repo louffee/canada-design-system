483c7495262f482b90f25d1901a4bd423554b99f
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-09-23 01:49:37 -0300

    refactor(typography): improve how the variant property acceptable values are inserted in the types

--- packages/@louffee-typography/src/TypographyProps.ts
+++ packages/@louffee-typography/src/TypographyProps.ts
@@ -1,11 +1,11 @@
 import type * as React from 'react'
-import type { LouThemeSchema } from '@louffee/canada-style-system'
 
 import type TypographyWeight from './TypographyWeight'
+import type TypographyVariant from './TypographyVariant'
 
 export default interface TypographyProps
   extends Omit<React.HTMLAttributes<HTMLElement>, 'style' | 'className' | 'color' | 'children'> {
-  variant: keyof LouThemeSchema['typography']['variants']
+  variant: TypographyVariant
   children: React.ReactNode
   weight?: TypographyWeight
 
