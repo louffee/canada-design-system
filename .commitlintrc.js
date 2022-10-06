58cabedac09dc05252010193f5403232fb9ce6ba
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-10-06 19:14:18 -0300

    chore(workspace): set commitlint to be stricter when it comes down to scope of commit messages

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,13 @@
+const fs = require('fs')
+
+const SCOPE_SETTING = '@louffee-'
+const packageNames = fs.readdirSync('./packages').map((name) => name.replace(SCOPE_SETTING, ''))
+
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-};
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-enum': [2, 'always', ['root', 'workspace', 'deps', 'dev-deps', 'release', 'workflow', ...packageNames]],
+    'scope-case': [2, 'always', 'kebab-case'],
+  },
+}
